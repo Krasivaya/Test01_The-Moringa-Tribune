08ad9dca108532b8ae960302addb888a49d1ac06
Krasivaya <semwagacarine@gmail.com>
2019-10-28 09:56:02 +0200

    Start test01

--- news/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Article,tags
-
-class ArticleAdmin(admin.ModelAdmin):
-    filter_horizontal = ('tags',)
-
-# admin.site.register(Editor)
-admin.site.register(Article)
-admin.site.register(tags)
--- virtual/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/wecode/Documents/moringa/core/django/week2/The-Moringa-Tribune/virtual/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
